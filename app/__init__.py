5805e4bfec7794f10abad8e711949f3331630f8b
Tye Wang <tye.wang@venmo.com>
2014-12-05 16:59:14 -0500

    Add migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask.ext.migrate import Migrate
 from flask.ext.sqlalchemy import SQLAlchemy
 
 flask_app = Flask(__name__)
 db = SQLAlchemy(flask_app)
+migrate = Migrate(flask_app, db)
 
 import controllers
--- tasks.py
+++ tasks.py
@@ -1,8 +1,9 @@
 import os
 
-from invoke import task, run
+from flask.ext.migrate import MigrateCommand
+from invoke import task, run, Collection
 
-from app import db
+from app import flask_app, db
 
 @task(default=True)
 def run_tests():
@@ -17,3 +18,18 @@ def clear_pyc():
 def reset_database():
     db.drop_all()
     db.create_all()
+
+@task
+def migrate(command):
+    MigrateCommand.app = flask_app
+    MigrateCommand.handle('', [command])
+
+namespace = Collection()
+for root_tasks in [run_tests, clear_pyc]:
+    namespace.add_task(root_tasks)
+
+db_tasks = Collection('db')
+for db_task in [reset_database, migrate]:
+    db_tasks.add_task(db_task)
+
+namespace.add_collection(db_tasks)
